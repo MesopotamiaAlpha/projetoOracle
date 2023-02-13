4856ed77e025826640f7d445dfec8db09d69a1c1
José Antonio <leao-gomes@hotmail.com>
2023-02-12 23:40:57 -0300

    Adição do menu

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, shell } = require('electron');
+const { app, BrowserWindow, shell, Menu } = require('electron');
 const timezone = require ("./js/timezone");
 const sqlplus = require ("./js/sqlplus");
 
@@ -7,6 +7,30 @@ const createWindow = () => {
     width: 800,
     height: 600,
   })
+  const template = [
+    {
+        label: 'Ajuda',
+        submenu: [{ // adds submenu
+                label: `Sobre`,
+            }, {
+                type: 'separator' // horizontal line between submenu items
+            },{
+                label: 'Sair',
+                role: 'quit' // closes app when clicked  
+
+            }]
+    },
+    {
+        label: 'Refresh', // Refreshes or reloads the page when clicked
+        role: 'reload'
+    },
+    {
+        label: 'Importar', // zooms the page when clicked
+        role : 'Importar'
+    }
+]
+const menu = Menu.buildFromTemplate (template) // sets the menu
+Menu.setApplicationMenu (menu)
 
   win.loadFile('index.html')
 }
